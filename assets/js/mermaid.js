45e3adb12f5a9f9006381cbee79ff9a85ea40068
varomix <varomix@gmail.com>
2026-06-09 15:24:39 -0600

    target mermaid foreignObject .label text with color:#000, revert lineColor to gray

--- assets/js/mermaid.js
+++ assets/js/mermaid.js
@@ -9,15 +9,13 @@ mermaid.initialize({
   themeVariables: {
     background: "#1a1e2a",
     primaryTextColor: "#000000",
-    nodeTextColor: "#000000",
-    tertiaryTextColor: "#000000",
     primaryColor: isDark ? "#6cb4ff" : "#6cb4ff",
     secondaryColor: isDark ? "#5efa9e" : "#5efa9e",
     tertiaryColor: isDark ? "#151820" : "#151820",
     primaryBorderColor: isDark ? "#6cb4ff" : "#6cb4ff",
     secondaryBorderColor: "#5efa9e",
     tertiaryBorderColor: isDark ? "#262d3d" : "#262d3d",
-    lineColor: "#000000",
+    lineColor: isDark ? "#7d849a" : "#7d849a",
     fontFamily:
       "ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,segoe ui,Roboto,helvetica neue,Arial,noto sans,sans-serif",
     fontSize: "16px",
@@ -26,7 +24,7 @@ mermaid.initialize({
     pieLegendTextSize: "16px",
     pieStrokeWidth: "1px",
     pieOuterStrokeWidth: "0.5px",
-    pieStrokeColor: "#000000",
+    pieStrokeColor: isDark ? "#7d849a" : "#7d849a",
     pieOpacity: "1",
   },
 });
